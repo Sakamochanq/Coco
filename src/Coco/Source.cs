59e38a5f4d9423088378d218907ec3a33fe29cdc
Sakamochanq <sakamochan123@outlook.jp>
2026-06-05 23:47:08 +0900

    feat: プロジェクトファイルの追加

---
+++ src/Coco/Source.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Coco
+{
+    public partial class Source : Form
+    {
+        public Source()
+        {
+            InitializeComponent();
+        }
+    }
+}
