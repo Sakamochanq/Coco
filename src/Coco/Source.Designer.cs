fca6cf68caee5408266101c3256ceab9b44e1c0b
Sakamochanq <sakamochan123@outlook.jp>
2026-06-06 12:23:24 +0900

    feat: UI-Componentが完成

--- src/Coco/Source.Designer.cs
+++ src/Coco/Source.Designer.cs
@@ -29,38 +29,160 @@
         private void InitializeComponent()
         {
             this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddObjectButton1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.DelObjectButton1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Objectproperty = new System.Windows.Forms.PropertyGrid();
+            this.ObjectListBox = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.AddObjectButton2 = new System.Windows.Forms.Button();
+            this.DelObjectButton2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox
             // 
             this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox.Location = new System.Drawing.Point(14, 41);
             this.pictureBox.Name = "pictureBox";
             this.pictureBox.Size = new System.Drawing.Size(704, 445);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(967, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddObjectButton1,
+            this.DelObjectButton1});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.ファイルFToolStripMenuItem.Text = "描画（D）";
+            // 
+            // AddObjectButton1
+            // 
+            this.AddObjectButton1.Name = "AddObjectButton1";
+            this.AddObjectButton1.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.A)));
+            this.AddObjectButton1.Size = new System.Drawing.Size(256, 22);
+            this.AddObjectButton1.Text = "新規オブジェクトの追加";
+            // 
+            // DelObjectButton1
+            // 
+            this.DelObjectButton1.Name = "DelObjectButton1";
+            this.DelObjectButton1.Size = new System.Drawing.Size(256, 22);
+            this.DelObjectButton1.Text = "選択中のオブジェクトを削除";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.DelObjectButton2);
+            this.panel1.Controls.Add(this.AddObjectButton2);
+            this.panel1.Controls.Add(this.ObjectListBox);
+            this.panel1.Controls.Add(this.Objectproperty);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(735, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 499);
+            this.panel1.TabIndex = 2;
+            // 
+            // Objectproperty
+            // 
+            this.Objectproperty.Location = new System.Drawing.Point(14, 251);
+            this.Objectproperty.Name = "Objectproperty";
+            this.Objectproperty.Size = new System.Drawing.Size(206, 236);
+            this.Objectproperty.TabIndex = 0;
+            // 
+            // ObjectListBox
+            // 
+            this.ObjectListBox.FormattingEnabled = true;
+            this.ObjectListBox.ItemHeight = 12;
+            this.ObjectListBox.Location = new System.Drawing.Point(14, 17);
+            this.ObjectListBox.Name = "ObjectListBox";
+            this.ObjectListBox.Size = new System.Drawing.Size(206, 160);
+            this.ObjectListBox.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 501);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(735, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // AddObjectButton2
+            // 
+            this.AddObjectButton2.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.AddObjectButton2.Location = new System.Drawing.Point(14, 183);
+            this.AddObjectButton2.Name = "AddObjectButton2";
+            this.AddObjectButton2.Size = new System.Drawing.Size(30, 30);
+            this.AddObjectButton2.TabIndex = 4;
+            this.AddObjectButton2.Text = "+";
+            this.AddObjectButton2.UseVisualStyleBackColor = true;
+            // 
+            // DelObjectButton2
+            // 
+            this.DelObjectButton2.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.DelObjectButton2.Location = new System.Drawing.Point(52, 184);
+            this.DelObjectButton2.Name = "DelObjectButton2";
+            this.DelObjectButton2.Size = new System.Drawing.Size(168, 30);
+            this.DelObjectButton2.TabIndex = 4;
+            this.DelObjectButton2.Text = "選択中のオブジェクトを削除";
+            this.DelObjectButton2.UseVisualStyleBackColor = true;
+            // 
             // Source
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(730, 471);
+            this.ClientSize = new System.Drawing.Size(967, 523);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Source";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Coco   |   Sakamochanq";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AddObjectButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem DelObjectButton1;
+        private System.Windows.Forms.PropertyGrid Objectproperty;
+        private System.Windows.Forms.ListBox ObjectListBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button DelObjectButton2;
+        private System.Windows.Forms.Button AddObjectButton2;
     }
 }
 
