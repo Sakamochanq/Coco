d458040e08cbe1a4a3f317e5a5296f09e4a1fec0
Sakamochanq <sakamochan123@outlook.jp>
2026-06-06 15:11:47 +0900

    fix: カテゴリを`Object`に変更

--- src/Coco/utils/device.cs
+++ src/Coco/utils/device.cs
@@ -10,15 +10,15 @@ namespace Coco.utils
         [ReadOnly(true)]
         public Rectangle Cord { get; set; }
 
-        [Category("Basic")]
+        [Category("Object")]
         [Description("機材の識別IDです")]
         public int ID { get; set; }
 
-        [Category("Basic")]
+        [Category("Object")]
         [Description("機材名です")]
         public string Name { get; set; }
 
-        [Category("Basic")]
+        [Category("Object")]
         [Description("使用者名です")]
         public string User { get; set; }
 
