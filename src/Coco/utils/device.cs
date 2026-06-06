bd7667c94a55c230f1015809ce154bfa3d724a84
Sakamochanq <sakamochan123@outlook.jp>
2026-06-06 15:27:48 +0900

    fix: カテゴリ名を自動翻訳へ対応

--- src/Coco/utils/device.cs
+++ src/Coco/utils/device.cs
@@ -5,25 +5,25 @@ namespace Coco.utils
 {
     internal class device
     {
-        [Category("Coordinate")]
+        [Category("Layout")]
         [Description("オブジェクトの座標・サイズです。")]
         [ReadOnly(true)]
         public Rectangle Cord { get; set; }
 
-        [Category("Object")]
+        [Category("Appearance")]
         [Description("機材の識別IDです")]
         public int ID { get; set; }
 
-        [Category("Object")]
+        [Category("Appearance")]
         [Description("機材名です")]
         public string Name { get; set; }
 
-        [Category("Object")]
+        [Category("Appearance")]
         [Description("使用者名です")]
         public string User { get; set; }
 
         [Category("Appearance")]
-        [DisplayName("色")]
+        [DisplayName("Color")]
         [Description("オブジェクトの背景色です")]
         public Color ObjectColor { get; set; } = Color.DeepSkyBlue;
 
